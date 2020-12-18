ac08f9de8403c334cd9fc5893e91683b7eff07b7
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2020-12-18 14:30:24 -0500

    commiting so I can pull

--- public/assets/js/api_script.js
+++ public/assets/js/api_script.js
@@ -2,21 +2,25 @@ $(document).ready(function () {
 
     // This code allows for an image to appear when the user hovers over the link of the
     // most popular strains
-    $(".strainImage").mouseenter(function () {
+    $(".strainImage").mouseenter(function () 
+    {
         // on mouse hover grab this. data-image and then append to strainInfo
         // download all the photos and have it loop through the photos to find the image that matches the data-name of the one that is being hovered over. 
         //either this.text or grabbing the data-name like you did below with li.image
 
-        if ($(this).parent('li').children('li.image').length) {
+        if ($(this).parent('li').children('li.image').length) 
+        {
             $("#strainImage").parent('li').children('li.image').show();
-        } else {
+        } 
+        else 
+        {
             var image_name = $(this).data('image');
             var imageTag = '<li class="image" >' + '<img src="' + image_name + '" alt="image" height="25" />' + '</li>';
             $("#strainImage").empty().append(imageTag);
         }
     });
-    
-// hides the image after moving 
+
+    // hides the image after moving 
     $("#strainImage").mouseleave(function () {
         $(this).parent('li').children('li.image').hide();
     });
@@ -27,7 +31,8 @@ $(document).ready(function () {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
+    }).then(function (response) 
+    {
         // This code allows for the first strain on the list to be displayed upon load of page.
         var preLoad = "OG Kush";
         var ulInfo = $("<ul>");
@@ -39,14 +44,16 @@ $(document).ready(function () {
         var liPos = $("<li>").html(response[preLoad].effects.positive);
         var nameNeg = $("<li>").html("Negative Effects: <br>").css("color", "dark green").css("font-weight", "bold");
         var liNeg = $("<li>").html(response[preLoad].effects.negative);
+        
+            ulInfo.append(nameStrain, liName, nameMed, liMed, namePos, liPos, nameNeg, liNeg).css("text-align", "left");
 
-        ulInfo.append(nameStrain, liName, nameMed, liMed, namePos, liPos, nameNeg, liNeg).css("text-align", "left");
+                $("#strainInfo").prepend(ulInfo)
 
-        $("#strainInfo").prepend(ulInfo)
 
-        // Created code to allow for the API info to be pulled and displayed based on what link the 
-        // user clicks on.
+        // Created code to allow for the API info to be pulled and displayed based on what link the user clicks on.
         $("#strainList").on("click", "li", function (e) {
+            //this empties the 
+            
             $("#strainInfo").empty();
             // Find what li was clicked on, get value
             var clicked = $(this).text();
@@ -65,23 +72,7 @@ $(document).ready(function () {
                 var nameNeg = $("<li>").html("Negative Effects: <br>").css("color", "dark green").css("font-weight", "bold");
                 var liNeg = $("<li>").html(response[data].effects.negative);
 
-                // var imgArray = [
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/171214_38919_OGkush_SAF_1-768x1024.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/180315_53294_PineappleExpress_1-225x300.jpg",
-                //    "https://images.weedmaps.com/pictures/users/004/737/179/original/71785049_171020_12477_Ak47_1.jpg?fit=fill&fill=solid&fill-color=%23FFFFFF&w=250&h=250",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/20171228_48822_F_BlueDream_1-1-225x300.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/171207_45517_Gelato_SAF_1-768x1024.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/171222_Bonsai-Cultivation_Green-Crack_1-1-300x300.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/180208_32587_PurpleHaze_1-768x1024.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/171207_45517_Gelato_SAF_1-1-768x1024.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/180308_47149_WeddingCake_1-225x300.jpg",
-                //    "https://weedmaps.com/news/wp-content/uploads/2020/05/171228_33422_Gelato_SAF_1-768x1024.jpg"
-                // ]
-
-
                 ulInfo.append(nameStrain, liName, nameMed, liMed, namePos, liPos, nameNeg, liNeg
-
-
                 ).css("text-align", "left");
                 $("#strainInfo").prepend(ulInfo);
 
