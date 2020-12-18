7094f60b4e4ada1d27cc457f8258f14b59369e0f
cpusillo <cpusillo@gmail.com>
2020-12-17 20:58:40 -0500

    Added all strains table

--- public/assets/js/addstrain.js
+++ public/assets/js/addstrain.js
@@ -3,12 +3,44 @@ $(document).ready(function () {
     var name = $("#name");
     var race = $("#race");
 
-        // Create a variable for our current user id.
-        var userId;
-        // GET our current user id and store it in userId.
-        $.get("/api/getuser", function(response){
-            userId = response;
-        });
+    // Create a variable for our current user id.
+    var userId;
+    // GET our current user id and store it in userId.
+    $.get("/api/getuser", function(response){
+        userId = response;
+    });
+
+    $.get("/api/allstrains", function(response){
+
+        // All table elements are appended here.
+        var table = $("<table>").addClass("table is-fullwidth");
+
+        // Build the table head structure
+        
+        var thead = $("<thead>");
+        var trHead = $("<tr>");
+        var th = $("<th>").text("ID");
+        var th2 = $("<th>").text("Name");
+        var th3 = $("<th>").text("Race");
+
+        thead.append(trHead).append(th).append(th2).append(th3);
+
+        for(var i = 0; i < response.length; i++){
+        var tbody = $("<tbody>");
+        var trBody = $("<tr>");
+        var tdName = $("<td>").text(response[i].name);
+        var tdRace = $("<td>").text(response[i].race);
+        var tdID = $("<td>").text(response[i].id);
+        
+        tbody.append(trBody).append(tdID).append(tdName).append(tdRace);
+
+        table.append(tbody);
+        table.append(thead);
+        }
+        // Append the finished table to the allStrains class already on the DOM.
+        $(".allStrains").append(table);
+
+    })
 
     // Handle the button click submission of our form fields.
     $("#submitBtn").on("click", function(e) {
