6455b2ceeda312a71888a5a41894c428af5fb01f
cpusillo <cpusillo@gmail.com>
2020-12-17 21:34:10 -0500

    Delete posts functionality working

--- public/assets/js/allposts.js
+++ public/assets/js/allposts.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    
+    // Handle delete button click
+    $(document).on("click", "button.delete", handlePostDelete);
 
     $.get("/api/allposts", function(data){
         //console.log("Test View All Posts: " + JSON.stringify(data))
@@ -13,11 +16,10 @@ $(document).ready(function () {
         var allPosts = $("#allPosts"); // Where all child elements should be appended to
 
         // The ancestor is where all tile parents need to be appended to
-        var ancestor = $("<div>").addClass("tile is-ancestor box").attr('id', 'printable'+data[i].id);
+        var ancestor = $("<div>").addClass("tile is-ancestor box").attr('data-id', data[i].id);
 
-        var controlsUL = $("<ul>").addClass("controls");
-        var trashLI = $("<li>").html("<a href='#'><i class='fas fa-trash-alt'></i></a>");
-        var printLI = $("<li>").html("<a href='#' id='printer'><i class='fas fa-print'></i></a>");
+        // Make the dynamic trash icon
+        var trashP = $("<p>").html("<button class='delete'><i class='fas fa-trash-alt'></i></button>");
 
         // tileParent is where all div elements need to be appended to
         var tileParent = $("<div>").addClass("tile is-4 is-vertical is-parent");
@@ -51,23 +53,29 @@ $(document).ready(function () {
         tileParentHor.appendTo(ancestor);
 
         // Append our delete & print buttons to the ancestor
-        controlsUL.append(trashLI).append(printLI);
-        ancestor.append(controlsUL);
+        ancestor.append(trashP);
 
         // Append our ancestor to a current DOM element
         ancestor.appendTo(allPosts);
         }
-
-        $("#printer").on("click", function(){
-            console.log($(this).closest(".is-ancestor").attr("id"));
-            var toPrint = $(this).closest(".is-ancestor").attr("id")
-        //     printJS({
-        //         printable: toPrint,
-        //         type: 'html',
-        //         targetStyle: ['*'],
-        //         header: 'Herbizzle | My Journal Entry'
-        //     })
-         })
     });
+
+    function handlePostDelete(){
+        var clicked = $(this).closest(".is-ancestor").attr("data-id");
+        deletePost(clicked);
+    }
+      // This function does an API call to delete posts
+    function deletePost(id) {
+        $.ajax({
+        method: "DELETE",
+        url: "/api/delete/" + id
+        })
+        .then(function() {
+            console.log("Deleted");
+        });
+
+        location.reload();
+    }
+
 
 });
